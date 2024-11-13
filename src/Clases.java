ffc5e1f3218d3c0e0c17b71f3685f366923c0b4d
Kathy Sagredo <sagredo.kathy@gmail.com>
2024-11-13 18:59:52 -0300

    Modificación en curso de clases

--- src/Clases.java
+++ src/Clases.java
@@ -4,6 +4,12 @@ class Persona{
     String Nombre;
     int Edad;
 
+    // Constructor
+    Persona(String nom, int ed){
+        Nombre = nom;
+        Edad = ed;
+    }
+
     // Metodo
     static void Saludo(String cNombre){
         System.out.println("Hola que tal " + cNombre);
@@ -14,13 +20,12 @@ class Persona{
 
 public class Clases {
     public static void main(String[] args) {
-        Persona oPersona1 = new Persona();
-        Persona oPersona2 = new Persona();
+        Persona oPersona1 = new Persona("Victor",40); //Se entrega argumentos gracias al constructor
+        Persona oPersona2 = new Persona("Ana",15);
 
-        oPersona1.Nombre = "Victor";
-        oPersona1.Edad = 40;
-        oPersona2.Nombre = "Ana";
-        oPersona2.Edad = 15;
+        //oPersona1.Nombre = "Victor";
+        //oPersona1.Edad = 40;
+
 
         System.out.println(oPersona1.Nombre + " - " + oPersona2.Nombre);
         Persona.Saludo(oPersona1.Nombre);
--- src/Import.java
+++ src/Import.java
@@ -1,8 +1,8 @@
 // import aprendizaje.ejemploClases
 public class Import {
     public static void main(String[] args) {
-        Persona oPer1 = new Persona();
-        oPer1.Nombre = "Raul";
+        Persona oPer1 = new Persona("Raul", 20);
+        //oPer1.Nombre = "Raul";
         Persona.Saludo(oPer1.Nombre);
     }
 }
